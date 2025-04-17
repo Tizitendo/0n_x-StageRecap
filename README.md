b97929c4202aeac70dd0832843f9bc4524c7cc3d
Onyx <50289874+Tizitendo@users.noreply.github.com>
2025-04-17 14:19:47 +0200

    v1.0

---
+++ README.md
@@ -0,0 +1,8 @@
+# Onyx-SinkFaster
+- Shows all interactables and drones after leaving a stage
+
+## Special Thanks To
+* The Return Of Modding Discord
+
+## Contact
+For questions or bug reports, you can find us in the [RoRR Modding Server](https://discord.gg/VjS57cszMq) @Onyx
--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 log.info("Successfully loaded " .. _ENV["!guid"] .. ".")
-mods["RoRRModdingToolkit-RoRR_Modding_Toolkit"].auto(false)
+mods["RoRRModdingToolkit-RoRR_Modding_Toolkit"].auto(true)
 PATH = _ENV["!plugins_mod_folder_path"]
 mods.on_all_mods_loaded(function()
     for k, v in pairs(mods) do
@@ -9,11 +9,11 @@ mods.on_all_mods_loaded(function()
     end
     params = {
         Version = 0.01,
-        ChatKey = 9,
+        ChatKey = -1,
         InteractableList = {
             oShop2 = 2,
             oRiftChest1 = 2,
-            oActivator = 2,
+            oActivator = 1,
             oBarrel1 = 1,
             oChest4 = 2,
             oTeleporterFake = 0,
@@ -26,18 +26,18 @@ mods.on_all_mods_loaded(function()
             oShrine1 = 2,
             oShrine2 = 2,
             oCommandFinal = 0,
-            oMedCab = 2,
+            oMedCab = 1,
             oShop1 = 2,
             oChestUtility2 = 2,
             oMedbay = 0,
-            oUseChest = 2,
+            oUseChest = 1,
             oRoboBuddybroken = 0,
             oDroneItem = 2,
             oHiddenHand = 0,
             oChestHealing1 = 2,
             oGunChest = 2,
             oTeleporterEpic = 0,
-            oDroneUpgrader = 2,
+            oDroneUpgrader = 1,
             oShopEquipment = 2,
             oBarrelEquipment = 2,
             oChestDamage2 = 2,
@@ -49,7 +49,7 @@ mods.on_all_mods_loaded(function()
             oChestDamage1 = 2,
             oChestUtility1 = 2,
             oShrine3 = 2,
-            oDroneRecycler = 2,
+            oDroneRecycler = 1,
             oChest2 = 2
         },
         droneList = {
@@ -83,26 +83,10 @@ Initialize(function()
         fade = 0
     end)
 
-    -- Callback.add(Callback.TYPE.onStageStart, "asdfgyutfg", function()
-    --     InteractableList = {}
-    --     stackList = {}
-    --     activatedList = {}
-    --     droneList = {}
-    -- end)
-
     gm.post_script_hook(gm.constants.instance_create_depth, function(self, other, result, args)
-        -- log.warning(result.value.object_name)
-        -- log.warning(result.value.object_index)
-        -- log.warning(result.value.active)
-        -- log.warning(result.value.sprite_index)
-        -- log.warning(result.value:get_object_index_self())
-        -- oCustomObject
-        -- result.value.sprite_index ~= -1
-        -- GM.object_get_parent(result.value.object_index) == gm.constants.pInteractableChest
-        if result.value.active == 0 and result.value.sprite_index ~= -1 and result.value:get_object_index_self() ~=
-            gm.constants.oBNoSpawn and result.value:get_object_index_self() and
-            GM.object_get_parent(result.value:get_object_index_self()) ~= gm.constants.pInteractableDrone then
-            -- log.warning(result.value.object_name)
+        if (result.value.active == 0 or GM.object_get_parent(result.value.object_index) == gm.constants.pInteractableChest) and 
+        result.value.sprite_index ~= -1 and result.value:get_object_index_self() ~= gm.constants.oBNoSpawn and 
+        GM.object_get_parent(result.value.object_index) ~= gm.constants.pInteractableDrone then
             if params.InteractableList[result.value.object_name] == nil then
                 params.InteractableList[result.value.object_name] = 0
                 Toml.save_cfg(_ENV["!guid"], params)
@@ -116,11 +100,6 @@ Initialize(function()
                 if interactable:get_object_index_self() == result.value:get_object_index_self() then
                     newtoList = false
                     stackList[i] = stackList[i] + 1
-                    -- if result.value.charges then
-                    --     stackList[i] = stackList[i] + interactable.charges
-                    -- else
-                    --     stackList[i] = stackList[i] + 1
-                    -- end
                 end
             end
             if result.value.charges then
@@ -144,13 +123,14 @@ Initialize(function()
             end
 
             local newdrone = true
-            for _, drone in ipairs(droneList) do
-                if result.value:get_object_index_self() == drone:get_object_index_self() then
+            for k, v in ipairs(Instance.find_all(result.value.object_index)) do
+                if v.x < 0 then
                     newdrone = false
                 end
             end
             if newdrone then
-                table.insert(droneList, result.value)
+                local player = Player.get_client()
+                table.insert(droneList, Object.wrap(result.value.object_index):create(-1000, 0).value)
             end
         end
     end)
@@ -158,39 +138,34 @@ Initialize(function()
     gm.pre_script_hook(gm.constants.interactable_set_active, function(self, other, result, args)
         -- get teleporter when interacting with it
         if self.object_index == gm.constants.oTeleporter or self.object_index == gm.constants.oTeleporterEpic then
-                for i, type in ipairs(InteractableList) do
-                    for _, interactable in ipairs(Instance.find_all(type:get_object_index_self())) do
-                        if _ == 1 then
-                            activatedList[i] = 0
-                        end
-
-                        if interactable:get_data().maxCharges ~= nil then
-                            if interactable.charges < 0 then
-                                activatedList[i] = activatedList[i] + interactable.charges/interactable:get_data().maxCharges
-                            end
-                            activatedList[i] = activatedList[i] +
-                                                   (interactable:get_data().maxCharges - interactable.charges) /
-                                                   interactable:get_data().maxCharges
-                        else
-                            if interactable.active ~= 0 then
-                                activatedList[i] = activatedList[i] + 1
-                            end
-                        end
+            for i, type in ipairs(InteractableList) do
+                for _, interactable in ipairs(Instance.find_all(type:get_object_index_self())) do
+                    if _ == 1 then
+                        activatedList[i] = 0
                     end
-                    if stackList[i] ~= #Instance.find_all(type:get_object_index_self()) then
-                        activatedList[i] = stackList[i] - #Instance.find_all(type:get_object_index_self())
+
+                    if interactable:get_data().maxCharges ~= nil then
+                        if interactable.charges < 0 then
+                            activatedList[i] = activatedList[i] + interactable.charges /
+                                                   interactable:get_data().maxCharges
+                        end
+                        activatedList[i] = activatedList[i] +
+                                               (interactable:get_data().maxCharges - interactable.charges) /
+                                               interactable:get_data().maxCharges
+                    else
+                        if interactable.active ~= 0 then
+                            activatedList[i] = activatedList[i] + 1
+                        end
                     end
                 end
+                if stackList[i] ~= #Instance.find_all(type:get_object_index_self()) then
+                    activatedList[i] = stackList[i] - #Instance.find_all(type:get_object_index_self())
+                end
+            end
         end
     end)
 
     gm.post_code_execute("gml_Object_oInit_Draw_64", function(self, other)
-        -- if gm._mod_game_getDirector() ~= -4 and gm._mod_game_getDirector().teleporter_active >= 4 then
-        --     local ViewWidth = gm.display_get_gui_width()
-        --     local ViewHeight = gm.display_get_gui_height()
-        --     gm.draw_rectangle_colour(ViewWidth * 0.1, ViewHeight * 0.1, ViewWidth * 0.9, ViewHeight * 0.9, 0, 0, 0, 0, false);
-        -- end
-
         if gm._mod_game_getDirector() ~= -4 and (gm._mod_game_getDirector().teleporter_active >= 4 or openRecap) then
             if fade < 1 then
                 fade = fade + 0.1
@@ -211,36 +186,31 @@ Initialize(function()
                     numInteractables = numInteractables + stackList[i + 1]
                 end
             end
-            for i = #InteractableList, #InteractableList + #droneList - 1 do
-                if droneList[i + 1] and params.droneList[droneList[i + 1].object_name] == 2 then
-                    numInteractables = numInteractables + #Instance.find_all(droneList[i - #InteractableList + 1]:get_object_index_self())
+            for k, v in ipairs(droneList) do
+                if v and params.droneList[v.object_name] == 2 then
+                    numInteractables = numInteractables + #Instance.find_all(v:get_object_index_self()) - 1
                 end
             end
-            gm.draw_text_transformed(ViewWidth * 0.63,
-                    ViewHeight * 0.29, "Stage Clear: "..math.floor((numActivated/numInteractables) * 100).."%",
-                    ViewHeight / 600, ViewHeight / 600, 0)
+            for i = #InteractableList, #InteractableList + #droneList - 1 do
+                if droneList[i + 1] and params.droneList[droneList[i + 1].object_name] == 2 then
+                    numInteractables = numInteractables +
+                                           #Instance.find_all(
+                            droneList[i - #InteractableList + 1]:get_object_index_self())
+                end
+            end
+            gm.draw_text_transformed(ViewWidth * 0.63, ViewHeight * 0.29,
+                "Stage Clear: " .. math.floor((numActivated / numInteractables) * 100) .. "%", ViewHeight / 600,
+                ViewHeight / 600, 0)
 
             for i = 0, #InteractableList - 1 do
-                -- local spriteScale = math.max(gm.sprite_get_width(InteractableList[i + 1].sprite_index),
-                    -- gm.sprite_get_height(InteractableList[i + 1].sprite_index)) + 50
-                -- spriteScale = ((gm.sprite_get_height(InteractableList[i + 1].sprite_index)*InteractableList[i + 1].image_yscale) ^1.6)
+                local spriteScale = math.max(gm.sprite_get_height(InteractableList[i + 1].sprite_index),
+                                    gm.sprite_get_width(InteractableList[i + 1].sprite_index)) ^ 1.6
 
-                local spriteScale = math.max(gm.sprite_get_height(InteractableList[i + 1].sprite_index), gm.sprite_get_width(InteractableList[i + 1].sprite_index))^1.6
-                -- spriteScale = (InteractableList[i + 1].image_yscale ^1.6) / 2
-                
-                -- log.warning(spriteScale)
-                -- gm.draw_sprite_ext(InteractableList[i + 1].sprite_index, 0,
-                --     ViewWidth * 0.29 + ViewWidth * ((i % 5) + 1) * 0.07,
-                --     ViewHeight * 0.25 + ViewHeight * (math.floor(i / 5) + 1) * 0.15, ViewHeight / spriteScale,
-                --     ViewHeight / spriteScale, 0, Color.WHITE, 1)
                 gm.draw_sprite_ext(InteractableList[i + 1].sprite_index, 0,
-                    ViewWidth * 0.29 + ViewWidth * ((i % 5) + 1) * 0.07,
-                    ViewHeight * 0.25 + ViewHeight * (math.floor(i / 5) + 1) * 0.15, ViewHeight / spriteScale + ViewHeight/1500,
-                    ViewHeight / spriteScale + ViewHeight/1500, 0, Color.WHITE, 1)
-                -- gm.draw_sprite_ext(InteractableList[i + 1].sprite_index, 0,
-                --     ViewWidth * 0.29 + ViewWidth * ((i % 5) + 1) * 0.07,
-                --     ViewHeight * 0.25 + ViewHeight * (math.floor(i / 5) + 1) * 0.15, ViewHeight / spriteScale,
-                --     ViewHeight / spriteScale, 0, Color.WHITE, 1)
+                                ViewWidth * 0.29 + ViewWidth * ((i % 5) + 1) * 0.07,
+                                ViewHeight * 0.25 + ViewHeight * (math.floor(i / 5) + 1) * 0.15,
+                                ViewHeight / spriteScale + ViewHeight / 1500, ViewHeight / spriteScale + ViewHeight / 1500, 0,
+                                Color.WHITE, 1)
 
                 gm.draw_set_colour(3714480)
                 if activatedList[i + 1] == stackList[i + 1] then
@@ -256,31 +226,32 @@ Initialize(function()
                 end
                 if activatedList[i + 1] / (spawns + 1) * 10 % 10 == 0 then
                     gm.draw_text_transformed(ViewWidth * 0.32 + ViewWidth * ((i % 5) + 1) * 0.07,
-                    ViewHeight * 0.27 + ViewHeight * (math.floor(i / 5) + 1) * 0.15,
-                    math.floor(activatedList[i + 1] / (spawns + 1)) .. "/" .. math.floor(stackList[i + 1] / (spawns + 1)),
-                    ViewHeight / 1200, ViewHeight / 1200, 0)
+                                            ViewHeight * 0.27 + ViewHeight * (math.floor(i / 5) + 1) * 0.15, 
+                                            math.floor(activatedList[i + 1] / (spawns + 1)) .. "/" .. math.floor(stackList[i + 1] / (spawns + 1)),
+                                            ViewHeight / 1200, ViewHeight / 1200, 0)
                 else
                     gm.draw_text_transformed(ViewWidth * 0.32 + ViewWidth * ((i % 5) + 1) * 0.07,
-                    ViewHeight * 0.27 + ViewHeight * (math.floor(i / 5) + 1) * 0.15,
-                    activatedList[i + 1] / (spawns + 1) .. "/" .. math.floor(stackList[i + 1] / (spawns + 1)),
-                    ViewHeight / 1200, ViewHeight / 1200, 0)
+                                            ViewHeight * 0.27 + ViewHeight * (math.floor(i / 5) + 1) * 0.15, 
+                                            activatedList[i + 1] / (spawns + 1) .. "/" .. math.floor(stackList[i + 1] / (spawns + 1)), 
+                                            ViewHeight / 1200,
+                                            ViewHeight / 1200, 0)
                 end
-
             end
             gm.draw_set_colour(Color.WHITE)
             for i = #InteractableList, #InteractableList + #droneList - 1 do
-                if droneList[i - #InteractableList + 1]:exists() and #Instance.find_all(droneList[i - #InteractableList + 1]:get_object_index_self()) > 0 then
+                if droneList[i - #InteractableList + 1] and 
+                #Instance.find_all(droneList[i - #InteractableList + 1]:get_object_index_self()) > 1 then
                     local spriteScale = math.max(gm.sprite_get_width(droneList[i - #InteractableList + 1].sprite_index),
-                        gm.sprite_get_height(droneList[i - #InteractableList + 1].sprite_index)) + 100
+                                        gm.sprite_get_height(droneList[i - #InteractableList + 1].sprite_index)) + 100
                     gm.draw_sprite_ext(droneList[i - #InteractableList + 1].sprite_index, 0,
-                        ViewWidth * 0.29 + ViewWidth * ((i % 5) + 1) * 0.07,
-                        ViewHeight * 0.25 + ViewHeight * (math.floor(i / 5) + 1) * 0.15, ViewHeight / 3 / spriteScale,
-                        ViewHeight / 3 / spriteScale, 0, Color.WHITE, 1)
+                                    ViewWidth * 0.29 + ViewWidth * ((i % 5) + 1) * 0.07,
+                                    ViewHeight * 0.25 + ViewHeight * (math.floor(i / 5) + 1) * 0.15, ViewHeight / 3 / spriteScale,
+                                    ViewHeight / 3 / spriteScale, 0, Color.WHITE, 1)
 
                     gm.draw_text_transformed(ViewWidth * 0.31 + ViewWidth * ((i % 5) + 1) * 0.07,
-                        ViewHeight * 0.28 + ViewHeight * (math.floor(i / 5) + 1) * 0.15, #Instance.find_all(
-                            droneList[i - #InteractableList + 1]:get_object_index_self()), ViewHeight / 600,
-                        ViewHeight / 600, 0)
+                                            ViewHeight * 0.28 + ViewHeight * (math.floor(i / 5) + 1) * 0.15, #Instance.find_all(
+                                            droneList[i - #InteractableList + 1]:get_object_index_self()) - 1, ViewHeight / 600,
+                                            ViewHeight / 600, 0)
                 end
             end
         end
@@ -308,7 +279,6 @@ gui.add_imgui(function()
 
         local collapse = ImGui.CollapsingHeader("Interactables")
         if collapse then
-            
             for i, interactable in pairs(params.InteractableList) do
                 if i ~= "oIfritTower" and i ~= "oBlockDestroy" and i ~= "oEfMine" and i ~= "oArtifactButton" and i ~=
                     "oDoor" and i ~= "oScreen" and i ~= "oCommand" and i ~= "oMushroomButton" and i ~= "oGaussActive" then
@@ -329,5 +299,5 @@ gui.add_imgui(function()
 end)
 
 gui.add_always_draw_imgui(function()
-    -- openRecap = ImGui.IsKeyDown(params.ChatKey)
+    openRecap = ImGui.IsKeyDown(params.ChatKey)
 end)
